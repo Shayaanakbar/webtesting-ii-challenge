b665734ec3dbb6c4c4ca09a37a2156b74fdf7f8d
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-23 16:56:25 -0500

    app updates

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -12,4 +12,46 @@ it('renders without crashing', () => {
 
 it('renders without crashing', () => {
   render(<App/>)
+});import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+xit('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  render(<App />);
+});
+
+describe('Dashboard buttons in App', () => {
+
+  it('striked out!!', () => {
+    const { getByText } = render(<App />);
+
+    const button = getByText(/Strikes:/i)
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    getByText(/Strikes: 0/i);
+  });
+
+  it('two strikes!!', () => {
+    const { getByText } = render(<App strike={2}/>);
+
+    const button = getByText(/Strikes:/i)
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    cleanup();
+    getByText(/Strikes: 2/i);
+  });
+
 });
--- client/src/baseball/Dashboard/Dashboard.js
+++ client/src/baseball/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
-mport React from 'react';
+import React from 'react';
 
 const Dashboard = props => {
-  // const {}
-
+  const {strike, ball, foul, hit } = props;
   return (
     <>
+      <h3>Dashboard</h3>
+      <button onClick={strike}>Strikes:</button>
+      <button onClick={ball}>Balls:</button>
+      <button onClick={foul}>Fouls:</button>
+      <button onClick={hit}> Hits:</button>
     </>
   )
 }
